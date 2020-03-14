9faae17b8b457b9a202f5a99e2af588c7aaf1578
Kyle Holliday <kyleholliday@users.noreply.github.com>
2020-03-14 01:19:52 -0400

    Add files via upload

---
+++ CountrySelector.js
@@ -0,0 +1,20 @@
+import useStats from "../utils/useStats";
+
+export default function CountrySelector() {
+  const countries = useStats('https://covid19.mathdro.id/api/countries');
+  console.log(countries);
+  if (!countries) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div>
+      <select>
+        {Object.entries(countries.countries).map(([country, code]) => (
+          <option key={code} value={code}>
+            {country}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
